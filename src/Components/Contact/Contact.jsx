8dc49ffda83c76ee6a662e14d1e7af660c9f4433
cs0312 <usolanki@cloudstrytech.com>
2024-04-11 00:48:02 +0530

    adf

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,130 +3,48 @@ import "../Contact/Contact.css";
 import FadeOnScroll from "../Animated/motion";
 
 function Contact() {
+  // const [text , settext] = useState('Send Message')
+   
+  
+
+  const ContactForm = async (e) => {
+    e.preventDefault();
+    // settext('wait a second')
+    var formData = new FormData(document.getElementById("sub-form"));
+     
+
+    try {
+      const response = await fetch(document.getElementById("sub-form").getAttribute("action"), {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log('Data saved to Google Sheets successfully!');
+        // Optionally, you can reset the form here
+        document.getElementById("sub-form").reset();
+        document.getElementById('submitButton').innerHTML='Send Message';
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error("There was an error!", error);
+      alert('There was a problem in sending the data! Sorry');
+      document.getElementById('submitButton').innerHTML='Send Message';
+    }
+  };
+
   return (
-    // <div>
-    //      <div className="container-xxl py-5" >
-    //   <div className="container">
-    //     <div class="row contact-info wow fadeInUp">
-
-    //       <div class="col-md-4">
-    //         <div class="contact-address rounded">
-    //           <FadeOnScroll animation='up'>
-
-    //            <h3><i title='Map' className='fa fa-map'></i></h3>
-    //           <address>Khanderao Bazar,Tarana 456665, <br/>
-    //             Madhya Pradesh , India
-    //           </address>
-    //           </FadeOnScroll>
-    //         </div>
-    //       </div>
-
-    //       <div class="col-md-4">
-    //         <div class="contact-phone">
-    //         <FadeOnScroll animation='up'>
-    //            <h3><i title='Phone' className='fa fa-phone'></i></h3>
-    //           <p><a href="tel:+91-7313683823">+91 7313683823</a></p>
-    //           </FadeOnScroll>
-    //         </div>
-    //       </div>
-
-    //       <div class="col-md-4">
-    //         <div class="contact-email">
-    //         <FadeOnScroll animation='up'>
-    //            <h3><i title='email' className='fa fa-envelope'></i></h3>
-    //           <p><a href="mailto:hr@cloudstrytech.com">hr@cloudstrytech.com</a></p>
-    //           </FadeOnScroll>
-    //         </div>
-    //       </div>
-
-    //     </div>
-    //     <div className="row g-5 ">
-
-    //       <div className="col-lg-6 wow fadeIn leftside-design" data-wow-delay="0.1s" >
-    //         <FadeOnScroll>
-    //          <h1 className="display-6 fw-normal mb-2 text-light  ">Contact With Us Now!</h1>
-    //          <p class="text-light text-muted mb-2  " style={{fontWeight:400}}>
-    //          Experience our proficiency! Learn why our clients are ecstatic to collaborate with us. Reach out to us to get started
-    //             </p>
-    //          <form >
-    //           <div className="row g-3">
-    //             <div className="col-12">
-    //               <div className="form-floating">
-    //                 <input type="text" className="form-control inp-field" id="name" name="name" placeholder="Your Name" required/>
-    //                 <label htmlFor="name" className='inp-field'>Your Name<span style={{ color: 'red' }}>*</span></label>
-    //                 <div id="errorName" style={{ fontSize: '.8rem', textAlign: 'left' }} className="mx-2 text-danger"></div>
-    //               </div>
-    //             </div>
-    //             <div className="col-12">
-    //               <div className="form-floating">
-    //                 <input type="email" className="form-control inp-field" id="email" name="email" placeholder="Your Email"  required/>
-    //                 <label htmlFor="email" className='inp-field'>Your Email<span style={{ color: 'red' }}>*</span></label>
-    //                 <div id="errorEmail" style={{ fontSize: '.8rem', textAlign: 'left' }} className="mx-2 text-danger"></div>
-    //               </div>
-    //             </div>
-
-    //             {/* <div className="col-md-6">
-    //               <div className="form-floating">
-    //                 <select className="form-select" id="title3" name="title3" aria-label="Select Title">
-    //                   <option selected value="default">Select Type<span style={{ color: 'red' }}>*</span></option>
-    //                   <option id='type1' value="General">General</option>
-    //                   <option id='type2' value="For Hiring">For Hiring</option>
-    //                 </select>
-    //                 <label htmlFor="title">Select Reference<span style={{ color: 'red' }}>*</span></label>
-    //                 <div id="Problemerrortype" style={{ fontSize: '.8rem', textAlign: 'left' }} className="mx-2 text-danger"></div>
-    //               </div>
-    //             </div> */}
-    //             <div className="col-12">
-    //               <div className="form-floating">
-    //                 <input type="text" className="form-control inp-field" id="subject" name="subject" placeholder="Subject"  />
-    //                 <label htmlFor="subject" className='inp-field'>Subject</label>
-    //               </div>
-    //             </div>
-    //             <div className="col-12">
-    //               <div className="form-floating">
-    //                 <textarea className="form-control  " placeholder="Leave a message here" style={{ height: '100px' }} id="message" name="message" required></textarea>
-    //                 <label htmlFor="message  " className='inp-field'>Message<span style={{ color: 'red' }}>*</span></label>
-    //                 <div id="errorMessage" style={{ fontSize: '.8rem', textAlign: 'left' }} className="mx-2 text-danger"></div>
-    //               </div>
-    //             </div>
-    //             <div className="col-12">
-    //               <input className="my-btn  py-2 px-3 me-3" id="submitButton" type="submit" value='Send Message'   />
-    //             </div>
-    //           </div>
-    //         </form>
-    //         </FadeOnScroll>
-    //       </div>
-    //       <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style={{ minHeight: '450px' }}>
-
-    //         <div className="position-relative  overflow-hidden h-100">
-
-    //         <iframe title='map' className="position-relative w-100 h-100 map-style" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3663.4514079067208!2d76.043037!3d23.335655!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3963670062b66555%3A0xfa5c9876e7c27b41!2sCloudstry%20Technology%20Private%20Limited!5e0!3m2!1sen!2sin!4v1711875625859!5m2!1sen!2sin"  frameBorder="1" style={{ minHeight: '450px', border: '0' }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
-    //         </div>
-
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    // </div>
-    <section class="py-3 py-md-5">
-      <div class="container">
-        <div class="row gy-3 gy-md-4 gy-lg-0 align-items-lg-center">
-          <div
-            className=" col-12 col-lg-6 wow fadeIn leftside-design"
-            data-wow-delay="0.1s"
-          >
+    <section className="py-3 py-md-5">
+      <div className="container">
+        <div className="row gy-3 gy-md-4 gy-lg-0 align-items-lg-center">
+          <div className=" col-12 col-lg-6 wow fadeIn leftside-design" data-wow-delay="0.1s">
             <FadeOnScroll>
-              <h1 className="display-6 fw-normal mb-2 text-light  ">
-                Contact With Us Now!
-              </h1>
-              <p
-                class="text-light text-muted mb-2  "
-                style={{ fontWeight: 400 }}
-              >
-                Experience our proficiency! Learn why our clients are ecstatic
-                to collaborate with us. Reach out to us to get started
+              <h1 className="display-6 fw-normal mb-2 text-light">Contact With Us Now!</h1>
+              <p className="text-light text-muted mb-2" style={{ fontWeight: 400 }}>
+                Experience our proficiency! Learn why our clients are ecstatic to collaborate with us. Reach out to us to get started
               </p>
-              <form>
+              <form id="sub-form" action="https://script.google.com/macros/s/AKfycbzkGVEvNzMyalZx_3QRhhy4Lt2NATuLyYC4GIxcJaH4Sy2fN_KV8pTaZ9n5orCcuBk/exec" method="post" onSubmit={ContactForm}>
                 <div className="row g-3">
                   <div className="col-12">
                     <div className="form-floating">
@@ -138,14 +56,7 @@ function Contact() {
                         placeholder="Your Name"
                         required
                       />
-                      <label htmlFor="name" className="inp-field">
-                        Your Name<span style={{ color: "red" }}>*</span>
-                      </label>
-                      <div
-                        id="errorName"
-                        style={{ fontSize: ".8rem", textAlign: "left" }}
-                        className="mx-2 text-danger"
-                      ></div>
+                      <label htmlFor="name" className="inp-field">Your Name<span style={{ color: "red" }}>*</span></label>
                     </div>
                   </div>
                   <div className="col-12">
@@ -158,17 +69,9 @@ function Contact() {
                         placeholder="Your Email"
                         required
                       />
-                      <label htmlFor="email" className="inp-field">
-                        Your Email<span style={{ color: "red" }}>*</span>
-                      </label>
-                      <div
-                        id="errorEmail"
-                        style={{ fontSize: ".8rem", textAlign: "left" }}
-                        className="mx-2 text-danger"
-                      ></div>
+                      <label htmlFor="email" className="inp-field">Your Email<span style={{ color: "red" }}>*</span></label>
                     </div>
                   </div>
-
                   <div className="col-12">
                     <div className="form-floating">
                       <input
@@ -178,29 +81,20 @@ function Contact() {
                         name="subject"
                         placeholder="Subject"
                       />
-                      <label htmlFor="subject" className="inp-field">
-                        Subject
-                      </label>
+                      <label htmlFor="subject" className="inp-field">Subject</label>
                     </div>
                   </div>
                   <div className="col-12">
                     <div className="form-floating">
                       <textarea
-                        className="form-control  "
+                        className="form-control"
                         placeholder="Leave a message here"
                         style={{ height: "100px" }}
                         id="message"
                         name="message"
                         required
                       ></textarea>
-                      <label htmlFor="message  " className="inp-field">
-                        Message<span style={{ color: "red" }}>*</span>
-                      </label>
-                      <div
-                        id="errorMessage"
-                        style={{ fontSize: ".8rem", textAlign: "left" }}
-                        className="mx-2 text-danger"
-                      ></div>
+                      <label htmlFor="message" className="inp-field">Message<span style={{ color: "red" }}>*</span></label>
                     </div>
                   </div>
                   <div className="col-12">
@@ -208,7 +102,8 @@ function Contact() {
                       className="my-btn  py-2 px-3 me-3"
                       id="submitButton"
                       type="submit"
-                      value="Send Message"
+                      value='Send Message'
+                      // onClick={ContactForm}
                     />
                   </div>
                 </div>
@@ -269,7 +164,7 @@ function Contact() {
                         class="link-secondary text-decoration-none"
                         href="mailto:hr@cloudstrytech.com"
                       >
-                        hr@cloudstrytech.co
+                        hr@cloudstrytech.com
                       </a>
                     </p>
                   </div>
